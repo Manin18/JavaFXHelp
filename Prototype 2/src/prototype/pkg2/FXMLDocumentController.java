4deb13eb5bdf33c40d7ba330b2107d83af13217a
Manin18 <42750332+Manin18@users.noreply.github.com>
2018-11-24 22:59:32 +0100

    Hejsa

---
+++ Prototype 2/src/prototype/pkg2/FXMLDocumentController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype.pkg2;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author test
+ */
+public class FXMLDocumentController implements Initializable {
+    
+   @FXML
+    private Button startBtn;
+
+    @FXML
+    private Button quitBtn;
+
+    @FXML
+    void QuitButton(ActionEvent event) {
+        Stage stage = (Stage) quitBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void StartButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GameStart.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ Prototype 2/src/prototype/pkg2/GameStartController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype.pkg2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author test
+ */
+public class GameStartController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private Button gangBtn;
+
+    @FXML
+    private Button inventoryBtn;
+
+    @FXML
+    void GangButton(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void InventoryButton(ActionEvent event) {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
